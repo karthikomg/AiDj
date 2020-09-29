18eb1986669f2f906baea15ced630b6d0ddd8902
karthikomg <71634294+karthikomg@users.noreply.github.com>
2020-09-29 20:46:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -58,33 +58,32 @@ function draw() {
 	{ 
 		circle(rightWristX,rightWristY,20);
 
-		if(rightWristY >0 && rightWristY <= 100)
+		if(rightWristX >0 && rightWristY <= 100)
 		{
 			document.getElementById("speed").innerHTML = "Speed = 0.5x";		
 			song.rate(0.5);
 		}
-		else if(rightWristY >100 && rightWristY <= 200)
+		else if(rightWristX >100 && rightWristY <= 200)
 		{
 			document.getElementById("speed").innerHTML = "Speed = 1x";		
 			song.rate(1);
 		}
-		else if(rightWristY >200 && rightWristY <= 300)
+		else if(rightWristX >200 && rightWristY <= 300)
 		{
 			document.getElementById("speed").innerHTML = "Speed = 1.5x";		
 			song.rate(1.5);
 		}
-		else if(rightWristY >300 && rightWristY <= 400)
+		else if(rightWristX >300 && rightWristY <= 400)
 		{
 			document.getElementById("speed").innerHTML = "Speed = 2x";		
 			song.rate(2);
 		}
-		else if(rightWristY >400)
+		else if(rightWristX >400)
 		{
 			document.getElementById("speed").innerHTML = "Speed = 2.5x";		
 			song.rate(2.5);
 		}
 	}
-
 	if(scoreLeftWrist > 0.2)
 	{
 		circle(leftWristX,leftWristY,20);
